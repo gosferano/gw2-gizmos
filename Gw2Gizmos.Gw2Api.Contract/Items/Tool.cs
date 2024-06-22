8d9cbcdec238f0c0025d8c4f1dd376278fbf194d
Gytis Barkauskas <gytisbark@gmail.com>
2024-06-22 13:03:08 +0300

    Fix Tool item contract

--- Gw2Gizmos.Gw2Api.Contract/Items/Tool.cs
+++ Gw2Gizmos.Gw2Api.Contract/Items/Tool.cs
@@ -1,6 +1,11 @@
 ﻿namespace Gw2Gizmos.Gw2Api.Contract.Items;
 
-public class Tool
+public class Tool : Item
+{
+    public ToolDetails Details { get; set; }
+}
+
+public class ToolDetails
 {
     public ToolType Type { get; set; }
     public int Charges { get; set; }
