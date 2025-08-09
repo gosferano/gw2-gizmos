6dcb2ebb2ba975dc51305c5d93b09f7499dbf126
gosferano <gosferano@outlook.com>
2025-08-09 04:30:23 +0300

    Initialize solution structure for data pipeline

---
+++ Gw2Gizmos.Sandbox/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Gw2Gizmos.Gw2Api.Client;
+using Gw2Gizmos.Gw2Api.Client.V2.Clients.Characters;
+using Gw2Gizmos.Gw2Api.Contract.Characters;
+
+var gw2ApiClient = new Gw2ApiClient(Environment.GetEnvironmentVariable("GW2_API_KEY"));
+var characters = await gw2ApiClient.V2.Characters.GetByIds(["Gosferano"]);
+var account = await gw2ApiClient.V2.Account.GetBlob();
+
+foreach (var guildId in account.GuildLeader)
+{
+    var guild = await gw2ApiClient.V2.Guild[guildId].GetBlob();
+    Console.WriteLine($"Guild Leader: {guildId}");
+}
