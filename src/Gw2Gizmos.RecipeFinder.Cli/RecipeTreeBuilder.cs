f9f5dc6d7ff248f0ce30c9dd28ee4f0512f63564
gosferano <gosferano@outlook.com>
2025-08-17 21:40:48 +0300

    Recipe Finder: memoize

--- src/Gw2Gizmos.RecipeFinder.Cli/RecipeTreeBuilder.cs
+++ src/Gw2Gizmos.RecipeFinder.Cli/RecipeTreeBuilder.cs
@@ -1,4 +1,6 @@
-﻿namespace Gw2Gizmos.RecipeFinder.Cli;
+﻿using System.Collections.Concurrent;
+
+namespace Gw2Gizmos.RecipeFinder.Cli;
 
 public class RecipeTreeBuilder
 {
@@ -57,8 +59,38 @@ public class RecipeTreeBuilder
             .ToList();
     }
 
+    private readonly ConcurrentDictionary<int, RecipeNode> _memoizationCache = new();
+
     public async Task<RecipeNode> BuildTreeAsync(int rootItemId, CancellationToken ct, int parentMultiplier = 1)
     {
+        if (_memoizationCache.TryGetValue(rootItemId, out var cachedNode))
+        {
+            // Return a deep copy of the cached node with updated count
+            return new RecipeNode
+            {
+                ItemId = cachedNode.ItemId,
+                ItemName = cachedNode.ItemName,
+                SellPrice = cachedNode.SellPrice * parentMultiplier,
+                BuyPrice = cachedNode.BuyPrice * parentMultiplier,
+                CraftingCost = cachedNode.CraftingCost * parentMultiplier,
+                IsProfitable = cachedNode.IsProfitable,
+                Count = cachedNode.Count * parentMultiplier,
+                Ingredients = cachedNode
+                    .Ingredients.Select(child => new RecipeNode
+                    {
+                        ItemId = child.ItemId,
+                        ItemName = child.ItemName,
+                        SellPrice = child.SellPrice * parentMultiplier,
+                        BuyPrice = child.BuyPrice * parentMultiplier,
+                        CraftingCost = child.CraftingCost * parentMultiplier,
+                        IsProfitable = child.IsProfitable,
+                        Count = child.Count * parentMultiplier,
+                        Ingredients = child.Ingredients
+                    })
+                    .ToList()
+            };
+        }
+
         var rootNode = new RecipeNode { ItemId = rootItemId, Count = parentMultiplier };
         var stack = new Stack<(RecipeNode Node, int ItemId, int Multiplier, bool Processed)>();
         stack.Push((rootNode, rootItemId, parentMultiplier, false));
@@ -81,6 +113,7 @@ public class RecipeTreeBuilder
                 currentNode.IsProfitable =
                     currentNode.CraftingCost > 0
                     && (currentNode.CraftingCost < currentNode.BuyPrice || currentNode.SellPrice == 0);
+
                 continue;
             }
 
@@ -104,13 +137,46 @@ public class RecipeTreeBuilder
                 foreach (var ingredient in recipe.Ingredients)
                 {
                     int childMultiplier = ingredient.Count * multiplier;
-                    var childNode = new RecipeNode { ItemId = ingredient.Id, Count = childMultiplier };
-                    currentNode.Ingredients.Add(childNode);
-                    stack.Push((childNode, ingredient.Id, childMultiplier, false));
+
+                    // Check the cache for the ingredient
+                    if (_memoizationCache.TryGetValue(ingredient.Id, out var cachedChildNode))
+                    {
+                        var childNode = new RecipeNode
+                        {
+                            ItemId = cachedChildNode.ItemId,
+                            ItemName = cachedChildNode.ItemName,
+                            SellPrice = cachedChildNode.SellPrice * childMultiplier,
+                            BuyPrice = cachedChildNode.BuyPrice * childMultiplier,
+                            CraftingCost = cachedChildNode.CraftingCost * childMultiplier,
+                            IsProfitable = cachedChildNode.IsProfitable,
+                            Count = cachedChildNode.Count * childMultiplier,
+                            Ingredients = cachedChildNode
+                                .Ingredients.Select(child => new RecipeNode
+                                {
+                                    ItemId = child.ItemId,
+                                    ItemName = child.ItemName,
+                                    SellPrice = child.SellPrice * childMultiplier,
+                                    BuyPrice = child.BuyPrice * childMultiplier,
+                                    CraftingCost = child.CraftingCost * childMultiplier,
+                                    IsProfitable = child.IsProfitable,
+                                    Count = child.Count * childMultiplier,
+                                    Ingredients = child.Ingredients
+                                })
+                                .ToList()
+                        };
+                        currentNode.Ingredients.Add(childNode);
+                    }
+                    else
+                    {
+                        var childNode = new RecipeNode { ItemId = ingredient.Id, Count = childMultiplier };
+                        currentNode.Ingredients.Add(childNode);
+                        stack.Push((childNode, ingredient.Id, childMultiplier, false));
+                    }
                 }
             }
         }
 
+        _memoizationCache[rootItemId] = rootNode;
         return rootNode;
     }
 }
